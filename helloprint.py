8765259ba26cffe221bc0196a763100b814582b6
Denre <denis.fahrutdinov95@gmail.com>
2023-05-23 16:28:57 +0700

    Первый код для проверки апдейтов в боте

--- helloprint.py
+++ helloprint.py
@@ -1,2 +1,28 @@
-print('Hello world')
-print('Hello too')
+import requests
+import time
+
+
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = ''
+TEXT: str = 'Ура! Классный апдейт!'
+MAX_COUNTER: int = 100
+
+offset: int = -2
+counter: int = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
