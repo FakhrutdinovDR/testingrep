e1c20e8a7c3c605aa302dae1fd947e70b236860b
Denis <skagents@yandex.ru>
2023-05-23 21:22:06 +0700

    Добавление гифок при апдейтах

--- helloprint.py
+++ helloprint.py
@@ -4,7 +4,7 @@ import time
 
 API_URL: str = 'https://api.telegram.org/bot'
 BOT_TOKEN: str = ''
-TEXT: str = 'Ура! Классный апдейт!'
+TEXT: str = 'Увы, кина не будет'
 MAX_COUNTER: int = 100
 
 offset: int = -2
@@ -22,7 +22,10 @@ while counter < MAX_COUNTER:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
-
+            getgif = requests.get(url='https://api.giphy.com/v1/gifs/random?api_key=PASTEYOURAPIKEY')
+            if getgif.status_code == 200:
+                requests.get(f"{API_URL}{BOT_TOKEN}/sendVideo?chat_id={chat_id}&video={getgif.json()['data']['images']['original_mp4']['mp4']}")
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
     time.sleep(1)
     counter += 1
