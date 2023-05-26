5799314b58d257ce367459e3533728db924a960e
Denre <denis.fahrutdinov95@gmail.com>
2023-05-26 14:02:21 +0700

    Доработана игра 1. Добавлена подсказка при вводе цифры 2. Сохранение в бинарном виде юзеров 3. Доработана логика работы игры, исключающая некоторые ошибки

--- gamenum/gameonbot.py
+++ gamenum/gameonbot.py
@@ -2,18 +2,31 @@ from gamenum.numbergame import Gameuser
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Text, Command
+import pickle
+import os
 
 BOT_TOKEN: str = ''
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 usersdict = {}
+if os.path.exists('baseusers.pkl'):
+    with open('baseusers.pkl', 'rb') as file:
+        data = pickle.load(file)
+        for key, value in data.items():
+            usersdict.setdefault(key, value)
+
+def writeupdateusers(): # Обновление инфы о юзерах
+    with open('baseusers.pkl', 'wb') as file:
+        pickle.dump(usersdict, file)
 
 @dp.message(Command(commands=['start']))
 async def proccessing_start(message: Message):
-    user = usersdict.setdefault(str(message.from_user.id), Gameuser(message.from_user.id))
+    usersdict.setdefault(str(message.from_user.id), Gameuser(message.from_user.id))
+    writeupdateusers()
     await message.answer(text='Добро пожаловать в бот по игре:\n'
                               'Угадай число от 1 до 100\n'
-                              'Инструкции - /help')
+                              'Инструкции - /help\n'
+                              'Хочешь начать играть? На любом этапе, если не играешь напиши да')
 
 @dp.message(Command(commands=['help']))
 async def proccessing_help(message: Message):
@@ -27,35 +40,36 @@ async def proccessing_help(message: Message):
 
 @dp.message(Command(commands=['cancel']))
 async def proccesing_cancelgame(message: Message):
-    usersdict[str(message.from_user.id)].cancelgame()
-    if usersdict[str(message.from_user.id)].statusingame == False:
+    user = usersdict[str(message.from_user.id)]
+    user.cancelgame() # Отмена игры
+    if not user.statusingame:
         await message.answer(text='Игра завершена')
     else:
         await message.answer(text='Что-то пошло не так, получил некорректный ответ от бэкенда')
 
 @dp.message(Command(commands=['stat']))
 async def proccessing_statistic(message: Message):
-    status = usersdict[str(message.from_user.id)].statusingame
-    countpop = f'Число попыток: {usersdict[str(message.from_user.id)].countchanses}'
+    user = usersdict[str(message.from_user.id)]
+    countpop = f'Число попыток: {user.countchanses}'
     await message.answer(text='Вот твоя статистика\n'
-                              f'{("Ты не в игре", "Ты в игре")[status]}\n'
-                              f'{("Счета попыток нет, ты не в игре", countpop)[status]}\n'
-                              f'Всего игр: {usersdict[str(message.from_user.id)].totalgames} из них побед: '
-                              f'{usersdict[str(message.from_user.id)].wins}')
+                              f'{("Ты не в игре", "Ты в игре")[user.statusingame]}\n'
+                              f'{("Счета попыток нет, ты не в игре", countpop)[user.statusingame]}\n'
+                              f'Всего игр: {user.totalgames} из них побед: '
+                              f'{user.wins}')
 
-@dp.message(Text(text=['Да', 'Хочу поиграть'], ignore_case=True))
+@dp.message(Text(text=['Да', 'Хочу поиграть', 'Давай', 'Сыграем'], ignore_case=True))
 async def startgame(message: Message):
-    status = usersdict[str(message.from_user.id)].statusingame
-    if status is False:
-        usersdict[str(message.from_user.id)].initgame()
+    user = usersdict[str(message.from_user.id)]
+    if not user.statusingame:
+        user.initgame()
         await message.answer(text='Игра началась, пиши только цифры от 1 до 100, цифру я загадал :)')
     else:
         await message.answer(text='Мы уже играем, пиши пожалуйста только цифры от 1 до 100')
 
-@dp.message(Text(text=['Нет'], ignore_case=True))
+@dp.message(Text(text=['Нет', 'Не хочу', 'Не буду', 'Давай потом'], ignore_case=True))
 async def answerno(message: Message):
-    status = usersdict[str(message.from_user.id)].statusingame
-    if status is True:
+    user = usersdict[str(message.from_user.id)]
+    if user.statusingame:
         await message.answer(text='Ты не закончил еще прошлую игру, вызови пожалуйста тогда /cancel')
     else:
         await message.answer(text='Жаль, возвращайся скорее! Чтобы начать, если захочешь, напиши мне "Хочу поиграть"')
@@ -63,12 +77,31 @@ async def answerno(message: Message):
 @dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
 async def game(message: Message):
     user = usersdict[str(message.from_user.id)]
-    if user.statusingame is True:
+    if user.statusingame:
         res = user.setstatus(int(message.text))
-        if res is True:
+        if res:
             await message.answer(text='Ты победил, можешь проверить статистику - /stat')
+            writeupdateusers()
+        if not res and user.countchanses is not None:
+            await message.answer(text=f'К сожалению, ты не угадал число, у тебя еще {user.countchanses} попыток')
+            if user.howmuch(int(message.text)):
+                await message.answer(text='Я бы попробовал число по выше')
+            else:
+                await message.answer(text='Я бы попробовал число по ниже')
+        if not res and user.countchanses is None:
+            await message.answer(text='Ты проиграл, попробуем еще раз? Да/Нет\n'
+                                      'Посмотреть статистику - /stat')
+            writeupdateusers()
     else:
-        await message.answer(text='Мы не начали играть, сходи сюда /help')
+        await message.answer(text='Мы не начали играть, сходи сюда /help, либо напиши хочу играть')
+
+@dp.message()
+async def proccessing_anyword(message: Message):
+    user = usersdict[str(message.from_user.id)]
+    if user.statusingame:
+        await message.answer(text='Идет игра, пиши только цифры пожалуйста, либо заверши игру - /cancel')
+    else:
+        await message.answer(text='Я умею только играть, если нужны инструкции - /help')
 
 if __name__ == '__main__':
     dp.run_polling(bot)
--- gamenum/numbergame.py
+++ gamenum/numbergame.py
@@ -5,27 +5,25 @@ class Gameuser:
         self.statusingame: bool = False
         self.totalgames: int = 0
         self.wins: int = 0
-        self.countchanses = None
+        self.countchanses = None # По умолчанию, если не в игре
 
-    def initgame(self):
+    def initgame(self): # Инициализация новой игры
         self.secrnum: int = randint(1, 101)
-        print(self.secrnum)
+        print(f'{self.userid}: {self.secrnum}') # В консоль загаднное число
         self.statusingame: bool = True
         self.totalgames += 1
         self.countchanses = 5
 
-    def validatenum(self, num: str):
-        if int(num) != self.secrnum:
-            return False
-        else:
-            return True
+    def validatenum(self, num: str): # Валидация введенного числа пользователем
+        return int(num) == self.secrnum
+
 
     def setstatus(self, num: str):
         resultbool = self.validatenum(num)
-        if resultbool is False and self.countchanses > 0:
+        if resultbool is False and 2 <= self.countchanses <= 5:
             self.countchanses -= 1
             return False
-        if resultbool is False and self.countchanses == 0:
+        if resultbool is False and self.countchanses == 1:
             self.statusingame = False
             self.countchanses = None
             return False
@@ -35,8 +33,10 @@ class Gameuser:
             self.countchanses = None
             return True
 
+    def howmuch(self, num):
+        return num < self.secrnum
 
-    def cancelgame(self):
+    def cancelgame(self): # Прерывание игры, количество игр остается тем же, пользователь проиграл отменив
         self.statusingame = False
         self.countchanses = None
 
